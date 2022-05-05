2013b1ef8c0257a0f1ac677cd7c16d492d5df33e
Dominic Yeboah <yeboahd24@gmail.com>
2022-05-05 07:39:08 -0700

    djoser test

---
+++ accounts/managers.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import BaseUserManager
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
